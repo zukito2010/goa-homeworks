86275d53bf26798316de97ec63fdf8ab1c40fcbb
zukito2010 <zuka.tughriashvili@gmail.com>
2024-05-16 17:17:46 +0400

    commit

---
+++ level 00/homework/cvladebi.py
@@ -0,0 +1,18 @@
+firstname = "Zuka"
+lastname = "Tughriashvili"
+asaki = "14"
+hobby = "Basketball,Viodeogames,Learning."
+height = "176"
+favcolor = "Red"
+
+
+print("This is",firstname,lastname,"he is",asaki,"years old and his hobbys are", hobby,"he is",height,"in height and his favourite color is",favcolor)
+
+
+
+
+
+
+
+
+
