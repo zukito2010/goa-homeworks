361f974eb8f16c7e71a3bc4ab9b9d481b9ca7b47
zukito2010 <zuka.tughriashvili@gmail.com>
2024-05-28 21:31:59 +0400

    commit

---
+++ level 05/homework/fstring.py
@@ -0,0 +1,29 @@
+# 2)
+
+name1 = "Kishvardi"
+name2 = "Jonny"
+name3 = "Temur"
+school = "G.O.A."
+
+print("Hi my name  is " + name1 + ". i am learning proggraming at " + school + " i have two cats first one is named" + name2 + "and the second one is " + name3 + ".")
+
+# in this code we are using variables instead of manually writing entire name.
+
+print("")
+
+
+
+# 3)
+
+
+
+
+variable = "variable"
+variable2 = 55
+animal = "cats"
+animal2 = "dogs"
+fstr = f"In fstring unlike normal string you can use {variable}s with just placying them inside brackets."
+fstr2 = f"you can also use numbers as an variable like {variable2}."
+fstr3 = f"you can even use other fstrings ''{fstr2}''"
+fav_animals = f"my favourite animals are {animal} and {animal2}."
+print(fstr,fstr2,fstr3,fav_animals)
